949b69f21c08da41fe06c39a594a851f696feeff
Innocent Nderitu <innocentbern22@gmail.com>
2023-11-28 13:37:37 +0300

    exported route

--- server/routes/user_routes.js
+++ server/routes/user_routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const {user_sign_up, user_login} = require("../controllers/user_controller");
+const requireUsertAuth = require("../middleware/requireUserAuth");
 
 //user sign_up route
 router.post('/sign_up', user_sign_up);
 
 //user login route
 router.post('/login', user_login);
+
+// require authentication routes below
+router.use(requireUsertAuth)
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 require("dotenv").config()
 
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors')
+const mongoose = require("mongoose");
+const cors = require('cors');
 
+const user_routes = require("./routes/user_routes");
 
 const app = express();
 
@@ -20,29 +21,13 @@ app.use((req, res, next) => {
 })
 
 // routes
+app.use("/api/user", user_routes);
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(process.env.MONGODB_URI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+//connect to db
+mongoose.connect(process.env.MONGODB_URI)
+    .then(()=> {
+        console.log("Connected to db");
+    })
 
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
